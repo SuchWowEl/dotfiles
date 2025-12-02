ae2320ee7c28f3bcf298e2223f24e7652df0a614
Elizer Dolorosa <zykisriel0434@gmail.com>
2025-12-03 06:45:46 +0800

    refactor: modify external monitors' brightness as well

--- dot_config/hypr/scripts/executable_brightness-control.sh
+++ dot_config/hypr/scripts/executable_brightness-control.sh
@@ -1,63 +1,85 @@
 #!/usr/bin/env bash
 
-# iDIR="$HOME/.config/mako/icons"
+# Get brightness for external monitor using ddcutil
+get_external_brightness() {
+	ddcutil getvcp 10 | awk -F'=' '/current value/ {gsub(/[^0-9]/,"",$2); print $2}'
+}
 
-# Get brightness
+# Set brightness for external monitor using ddcutil
+set_external_brightness() {
+	local change="$1"
+	local cur_brightness=$(get_external_brightness)
+	local new_brightness=$((cur_brightness + change))
+	# Clamp between 0 and 100
+	if (( new_brightness < 0 )); then new_brightness=0; fi
+	if (( new_brightness > 100 )); then new_brightness=100; fi
+	ddcutil setvcp 10 "$new_brightness"
+	notify-send -h string:x-canonical-private-synchronous:sys-notify -u low "External Monitor Brightness : ${new_brightness}%"
+}
+
+# Get brightness for internal monitor using brightnessctl
+get_internal_brightness() {
+	brightnessctl --device=amdgpu_bl1 g
+}
+
+# Set brightness for internal monitor using brightnessctl
+set_internal_brightness() {
+	local op="$1"
+	brightnessctl --device=amdgpu_bl1 set "$op" && notify-send -h string:x-canonical-private-synchronous:sys-notify -u low "Internal Monitor Brightness : $(brightnessctl --device=amdgpu_bl1 -m | awk -F, '{print substr($4, 0, length($4)-1)}')%"
+}
+
+# Main logic
+get_focused_monitor() {
+	MONITOR_NAME=$(hyprctl monitors -j | jq -r '.[] | select(.focused==true) | .name')
+	echo "$MONITOR_NAME"
+}
+
 get_backlight() {
-	LIGHT="$(cat /sys/class/leds/*::kbd_backlight/brightness)"
-	echo "${LIGHT}"
+	FOCUSED_MONITOR=$(get_focused_monitor)
+	if [[ "$FOCUSED_MONITOR" == "eDP-1" ]]; then
+		get_internal_brightness
+	else
+		get_external_brightness
+	fi
 }
 
-# Get icons
-# get_icon() {
-# 	current="$(cat /sys/class/leds/*::kbd_backlight/brightness)"
-#
-# 	if [[ ("$current" -ge "0") && ("$current" -le "1") ]]; then
-# 		icon="$iDIR/brightness-20.png"
-# 	elif [[ ("$current" -ge "1") && ("$current" -le "2") ]]; then
-# 		icon="$iDIR/brightness-60.png"
-# 	elif [[ ("$current" -ge "2") && ("$current" -le "3") ]]; then
-# 		icon="$iDIR/brightness-100.png"
-# 	fi
-# }
-
-# Notify
-notify_user() {
-	notify-send -h string:x-canonical-private-synchronous:sys-notify -u low "Keyboard Brightness : $(brightnessctl -d '*::kbd_backlight' g)"
-}
-
-# Increase brightness
 inc_backlight() {
-	brightnessctl s +5% && notify_user
+	FOCUSED_MONITOR=$(get_focused_monitor)
+	if [[ "$FOCUSED_MONITOR" == "eDP-1" ]]; then
+		set_internal_brightness "+5%"
+	else
+		set_external_brightness 5
+	fi
 }
 
-# Decrease brightness
 dec_backlight() {
-	brightnessctl s 5%- && notify_user
+	FOCUSED_MONITOR=$(get_focused_monitor)
+	if [[ "$FOCUSED_MONITOR" == "eDP-1" ]]; then
+		set_internal_brightness "5%-"
+	else
+		set_external_brightness -5
+	fi
 }
 
 # # Zero brightness
 # zero_backlight() {
-# 	brightnessctl -d *::kbd_backlight s 0%
+# 	DEV=$(get_device_name "$(get_focused_monitor)")
+# 	brightnessctl --device="$DEV" set 0%
 # }
-#
+
 # # Full brightness
 # full_backlight() {
-# 	brightnessctl -d *::kbd_backlight s 100%
+# 	DEV=$(get_device_name "$(get_focused_monitor)")
+# 	brightnessctl --device="$DEV" set 100%
 # }
 
 # Execute accordingly
 if [[ "$1" == "--get" ]]; then
-	brightnessctl -d '*::kbd_backlight' g
+	get_backlight
 elif [[ "$1" == "--inc" ]]; then
 	inc_backlight
 elif [[ "$1" == "--dec" ]]; then
 	dec_backlight
-# elif [[ "$1" == "--zero" ]]; then
-# 	zero_backlight
-# elif [[ "$1" == "--full" ]]; then
-# 	full_backlight
-
 else
 	get_backlight
 fi
